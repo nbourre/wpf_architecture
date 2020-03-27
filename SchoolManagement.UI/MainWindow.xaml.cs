cc40b49970cfa9841eb094f2b579b0e4562d3bf2
Nick B <nbourrereg@hotmail.com>
2020-03-27 16:26:15 -0400

    MVVM with Event in code-behind

--- SchoolManagement.UI/MainWindow.xaml.cs
+++ SchoolManagement.UI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using SchoolManagement.UI.ViewModels;
+﻿using SchoolManagement.Models;
+using SchoolManagement.UI.ViewModels;
 using System.Windows;
 
 namespace SchoolManagement.UI
@@ -8,11 +9,33 @@ namespace SchoolManagement.UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        StudentsViewModel _vm;
+
         public MainWindow(StudentsViewModel viewModel)
         {
             InitializeComponent();
 
             DataContext = viewModel;
+            _vm = viewModel;
+        }
+
+        private void NewRecord_Click(object sender, RoutedEventArgs e)
+        {
+            Student student = new Student() { FirstName = "Undefined", LastName = "Undefined", RegistrationNumber = "0000000"};
+            _vm.Students.Add(student);
+        }
+
+        private void Modify_Click(object sender, RoutedEventArgs e)
+        {
+            
+            StudentModify wnd = new StudentModify((Student)dg.SelectedItem);
+
+            wnd.ShowDialog();
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            _vm.Students.Remove((Student)dg.SelectedItem);
         }
     }
 }
